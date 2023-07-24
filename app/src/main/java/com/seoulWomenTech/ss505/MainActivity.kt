38ce98ad5df2d548d0cdc7f1ef2748b0d788dae6
iiolo <alohalo98@naver.com>
2023-07-24 20:52:49 +0900

    first project setting

---
+++ app/src/main/java/com/seoulWomenTech/ss505/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.seoulWomenTech.ss505
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
