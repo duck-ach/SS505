cc782506cf005d8b0949d3053334e8a654ad96f5
iiolo <alohalo98@naver.com>
2023-07-27 00:08:56 +0900

    refactor : 코드 간소화 및 mainActivity 디자인 주석 제거 - binding -> activityMainBinding으로 이름 변경 - activityMainBinding 중복 간소화

--- app/src/main/java/com/seoulWomenTech/ss505/MainActivity.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/MainActivity.kt
@@ -14,51 +14,20 @@ import com.google.android.material.transition.MaterialSharedAxis
 import com.seoulWomenTech.ss505.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    lateinit var viewPager2: ViewPager2
-    private val tabTextList = listOf("Profile", "Search", "Setting")
+    lateinit var activityMainBinding: ActivityMainBinding
+    private val tabTextList = listOf("챌린지", "게시판", "나의 챌린지")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        viewPager2 = binding.pager
-        viewPager2.adapter = ViewPagerAdapter(this)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        activityMainBinding.run {
+            pager.adapter = ViewPagerAdapter(this@MainActivity)
 
+            TabLayoutMediator(tabLayoutMain, pager) { tab, pos ->
+                tab.text = tabTextList[pos]
 
-
-        TabLayoutMediator(binding.tabLayoutMain, binding.pager) { tab, pos ->
-            tab.text = tabTextList[pos]
-
-        }.attach()
-
-        // 처음, 마지막 페이지간 양방향 이동 가능
-        binding.pager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
-            var currentState = 0
-            var currentPos = 0
-
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-                if(currentState == ViewPager2.SCROLL_STATE_DRAGGING && currentPos == position) {
-                    if(currentPos == 0) binding.pager.currentItem = 2
-                    else if(currentPos == 2) binding.pager.currentItem = 0
-                }
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-            }
-
-            override fun onPageSelected(position: Int) {
-                currentPos = position
-                Log.d("위치",currentPos.toString())
-                super.onPageSelected(position)
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-                currentState = state
-                super.onPageScrollStateChanged(state)
-            }
-        })
+            }.attach()
+        }
 
 
     }
