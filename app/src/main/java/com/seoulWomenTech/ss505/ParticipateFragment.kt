61e5ab9722b147cc485bd7c37fa9ef733c85b47e
iiolo <alohalo98@naver.com>
2023-07-28 19:45:27 +0900

    feat : 참여자 목록 구현

--- app/src/main/java/com/seoulWomenTech/ss505/ParticipateFragment.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/ParticipateFragment.kt
@@ -5,7 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.seoulWomenTech.ss505.databinding.FragmentParticipateBinding
+import com.seoulWomenTech.ss505.databinding.RowChallengeBinding
+import com.seoulWomenTech.ss505.databinding.RowParticipateBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,12 +63,61 @@ class ParticipateFragment : Fragment() {
                 }
             }
 
+
             participateMaxUser.append("( ${participantsList.size} / ${challenge.maxUser} )")
+
+            recyclerViewParticipate.run {
+                adapter = ParticipateRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(mainActivity)
+                addItemDecoration(DividerItemDecoration(mainActivity, DividerItemDecoration.VERTICAL))
+            }
 
         }
 
         return fragmentParticipateBinding.root
     }
+
+    inner class ParticipateRecyclerViewAdapter : RecyclerView.Adapter<ParticipateRecyclerViewAdapter.ParticipateViewHolderClass>(){
+
+        inner class ParticipateViewHolderClass(rowParticipateBinding: RowParticipateBinding) : RecyclerView.ViewHolder(rowParticipateBinding.root){
+            var textViewRowParticipateUserName : TextView
+
+            init {
+                textViewRowParticipateUserName  = rowParticipateBinding.participateRowUserName
+            }
+
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParticipateViewHolderClass {
+            val rowParticipateBinding = RowParticipateBinding.inflate(layoutInflater)
+            val ParticipateViewHolderClass = ParticipateViewHolderClass(rowParticipateBinding)
+
+            rowParticipateBinding.root.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            return ParticipateViewHolderClass
+        }
+
+        override fun getItemCount(): Int {
+            return participantsList.size
+        }
+
+        override fun onBindViewHolder(holder: ParticipateViewHolderClass, position: Int) {
+            holder.textViewRowParticipateUserName.text = participantsList[position].name
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        // 리사이클러뷰 갱신
+        fragmentParticipateBinding.recyclerViewParticipate.adapter?.notifyDataSetChanged()
+    }
+
 
 
 }
