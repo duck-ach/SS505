ca8fefa65989f3c67f1a6038cdb50eca2d01c008
iiolo <alohalo98@naver.com>
2023-07-30 21:59:19 +0900

    feat : CLG_ID로 CPI(인증 샷) 조회하는 메서드 추가

--- app/src/main/java/com/seoulWomenTech/ss505/CpiDAO.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/CpiDAO.kt
@@ -64,6 +64,52 @@ class CpiDAO {
             return CPIClass
         }
 
+
+        // Read by CLG_ID : CLG_ID로 데이터를 검색하는 메서드
+        fun selectDataByClgId(context: Context, clg_id: Int): MutableList<CPIClass> {
+            val dbHelper = DBHelper(context)
+            val selection = "CLG_ID = ?"
+            val args = arrayOf("$clg_id")
+            val db = dbHelper.writableDatabase
+
+            val cursor = db.query("CPI", null, selection, args, null, null, null)
+
+            val dataList = mutableListOf<CPIClass>()
+
+            if (cursor.moveToFirst()) {
+                val idx1 = cursor.getColumnIndex("CPI_ID")
+                val idx2 = cursor.getColumnIndex("CLG_ID")
+                val idx3 = cursor.getColumnIndex("USER_ID")
+                val idx4 = cursor.getColumnIndex("CPI_URL")
+                val idx5 = cursor.getColumnIndex("CPI_UN")
+                val idx6 = cursor.getColumnIndex("CPI_CN")
+
+                val cpi_id = cursor.getInt(idx1)
+                val clg_id = cursor.getInt(idx2)
+                val user_id = cursor.getInt(idx3)
+                val cpi_url = cursor.getString(idx4)
+                val cpi_un = cursor.getString(idx5)
+                val cpi_cn = cursor.getString(idx6)
+
+                val CPIClass = CPIClass(
+                    cpi_id,
+                    clg_id,
+                    user_id,
+                    cpi_url,
+                    cpi_un,
+                    cpi_cn
+                )
+
+                dataList.add(CPIClass)
+            }
+
+            cursor.close()
+            db.close()
+            
+            return dataList
+        }
+
+
         // Read All : 모든 행을 가져온다
         fun selectAllData(context: Context): MutableList<CPIClass> {
             val dbHelper = DBHelper(context)
