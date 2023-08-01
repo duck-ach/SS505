c0b0e32b9cd9d18bca41ebf0073f93d849fd45d0
iiolo <alohalo98@naver.com>
2023-08-01 14:22:41 +0900

    feat : 인증샷 제출 시 firebase storage에 올리는 기능 추가

--- app/src/main/java/com/seoulWomenTech/ss505/CPIFragment.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/CPIFragment.kt
@@ -1,15 +1,25 @@
 package com.seoulWomenTech.ss505
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.storage.FirebaseStorage
 import com.seoulWomenTech.ss505.databinding.FragmentCPIBinding
 
 class CPIFragment : Fragment() {
     lateinit var fragmentCPIBinding: FragmentCPIBinding
     lateinit var mainActivity: MainActivity
+
+    // 앨범 액티비티를 실행하기 위한 런처
+    lateinit var cpiLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -18,6 +28,7 @@ class CPIFragment : Fragment() {
         mainActivity = activity as MainActivity
 
         val cpiChallenge = ChallengeDAO.selectData(mainActivity,mainActivity.rowPosition)
+
 
         fragmentCPIBinding.run {
             toolbarCPI.run{
@@ -36,6 +47,31 @@ class CPIFragment : Fragment() {
             CPIMaxUser.append("${cpiChallenge.maxUser}명")
             CPILocation.append(cpiChallenge.location)
             CPIContent.append(cpiChallenge.content)
+
+
+            val contract1 = ActivityResultContracts.StartActivityForResult()
+            cpiLauncher = registerForActivityResult(contract1){
+                if(it?.resultCode == AppCompatActivity.RESULT_OK){
+                    val storage = FirebaseStorage.getInstance()
+                    val fileName = "image/cpi_${System.currentTimeMillis()}.jpg"
+                    val fileRef = storage.reference.child(fileName)
+                    fileRef.putFile(it.data?.data!!).addOnCompleteListener{
+                        Snackbar.make(fragmentCPIBinding.root, "업로드가 완료되었습니다", Snackbar.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
+
+            btnCPI.setOnClickListener {
+                val newIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                newIntent.setType("image/*")
+                val mimeType = arrayOf("image/*")
+                newIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+                cpiLauncher.launch(newIntent)
+//
+//                val cpiClass = CPIClass(0,cpiChallenge.idx,mainActivity.userPosition,)
+//                CpiDAO.insertData(mainActivity,cpiClass)
+            }
         }
 
         return fragmentCPIBinding.root
