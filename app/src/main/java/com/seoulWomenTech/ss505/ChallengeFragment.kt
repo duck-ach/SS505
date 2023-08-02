3b35f8c074ff94c4a09ba2d263322186ddbb99d5
iiolo <alohalo98@naver.com>
2023-08-02 15:11:40 +0900

    refactor : 챌린지, 마이챌린지 페이지 챌린지 목록 항목 디자인 변경, 데이터 추가 연결

--- app/src/main/java/com/seoulWomenTech/ss505/ChallengeFragment.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/ChallengeFragment.kt
@@ -1,5 +1,6 @@
 package com.seoulWomenTech.ss505
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.seoulWomenTech.ss505.databinding.FragmentChallengeBinding
 import com.seoulWomenTech.ss505.databinding.RowChallengeBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 
 class ChallengeFragment : Fragment() {
@@ -49,13 +54,24 @@ class ChallengeFragment : Fragment() {
 
         inner class ChallengeViewHolderClass(rowChallengeBinding: RowChallengeBinding) : RecyclerView.ViewHolder(rowChallengeBinding.root){
             var textViewRowChallengeDeadLine: TextView
+            var textViewRowChallengeDday: TextView
             var textViewRowChallengeTitle: TextView
             var challengeBtnParticipate : Button
+            var textViewRowChallengeDate : TextView
+            var textViewRowChallengeLocation : TextView
+            var textViewRowChallengeParticipant : TextView
+
 
             init {
                 textViewRowChallengeDeadLine = rowChallengeBinding.challengeRowDeadLine
+                textViewRowChallengeDday = rowChallengeBinding.challengeRowDday
                 textViewRowChallengeTitle = rowChallengeBinding.challengeRowTitle
                 challengeBtnParticipate = rowChallengeBinding.challengeBtnParticipate
+
+                textViewRowChallengeDate = rowChallengeBinding.challengeRowDate
+                textViewRowChallengeLocation = rowChallengeBinding.challengeRowLocation
+                textViewRowChallengeParticipant = rowChallengeBinding.challengeRowParticipant
+
                 challengeBtnParticipate.setOnClickListener {
                     mainActivity.rowPosition = challengeList[adapterPosition].idx
                     mainActivity.replaceFragment(MainActivity.PARTICIPATE_FRAGMENT, true, null)
@@ -82,9 +98,26 @@ class ChallengeFragment : Fragment() {
             return challengeList.size
         }
 
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onBindViewHolder(holder: ChallengeViewHolderClass, position: Int) {
             holder.textViewRowChallengeDeadLine.text = challengeList[position].progDate
+
+
+            val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+            val givenDate = LocalDate.parse(challengeList[position].progDate, formatter)
+
+            // 현재 날짜
+            val currentDate = LocalDate.now()
+
+            // D-day 계산
+            val daysUntilGivenDate = ChronoUnit.DAYS.between(currentDate, givenDate)
+
+            holder.textViewRowChallengeDday.setText("D - $daysUntilGivenDate")
+
             holder.textViewRowChallengeTitle.text = challengeList[position].name
+            holder.textViewRowChallengeDate.setText("일시 : ${challengeList[position].progDate} ${challengeList[position].progTime}")
+            holder.textViewRowChallengeLocation.setText("위치 : ${challengeList[position].location}")
+            holder.textViewRowChallengeParticipant.setText("모집 인원 : ${challengeList[position].maxUser}명")
         }
     }
 
