e62d852e3668c68ab61826153be67867a28e8183
crystal11111 <crystal031230@gmail.com>
2023-08-05 04:34:41 +0900

    feat: 안전자료 RecyclerView

--- app/src/main/java/com/seoulWomenTech/ss505/SafetyFragment.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/SafetyFragment.kt
@@ -1,59 +1,44 @@
 package com.seoulWomenTech.ss505
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.seoulWomenTech.ss505.Adapter.SafetyResourceAdapter
+import com.seoulWomenTech.ss505.databinding.FragmentSafetyBinding
+import com.seoulWomenTech.ss505.model.SafetyResourceModelClass
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SafetyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SafetyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    val binding by lazy {
+        FragmentSafetyBinding.inflate(layoutInflater)
+    }
+    private var safetydatalist = ArrayList<SafetyResourceModelClass>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        // 임의의 데이터들
+        safetydatalist.add(SafetyResourceModelClass(R.drawable.cpr, "심폐소생술 안전자료"))
+        safetydatalist.add(SafetyResourceModelClass(R.drawable.cpr, "심폐소생술 안전자료"))
+        safetydatalist.add(SafetyResourceModelClass(R.drawable.video, "비디오 1 입니다."))
+        safetydatalist.add(SafetyResourceModelClass(R.drawable.video, "비디오 2 입니다."))
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_safety, container, false)
+        binding.SafetyRecyclerView.layoutManager=LinearLayoutManager(requireContext())
+        var adaptor = SafetyResourceAdapter(safetydatalist)
+        binding.SafetyRecyclerView.adapter = adaptor
+        binding.SafetyRecyclerView.setHasFixedSize(true)
+
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SafetyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SafetyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
