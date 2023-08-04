15e23eb04f602e979384b6dc92496bf7a5789adc
crystal11111 <crystal031230@gmail.com>
2023-08-04 23:36:39 +0900

    crystal

--- app/src/main/java/com/seoulWomenTech/ss505/MainActivity.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.seoulWomenTech.ss505
 
-
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.tabs.TabLayoutMediator
 import com.google.android.material.transition.MaterialSharedAxis
 import com.seoulWomenTech.ss505.databinding.ActivityMainBinding
 
@@ -16,16 +14,17 @@ class MainActivity : AppCompatActivity() {
     // 사용자가 누른 항목 번호
     var rowPosition = 0
 
-    var newFragment:Fragment? = null
-    var oldFragment:Fragment? = null
-
+    var newFragment: Fragment? = null
+    var oldFragment: Fragment? = null
 
 
     companion object {
         // Activity가 관리할 프래그먼트들의 이름
         val MAIN_FRAGMENT = "MainFragment" //  메인 화면
         val PARTICIPATE_FRAGMENT = "ParticipateFragment" //  참여 화면
-
+        val SAFETY_FRAGMENT = "SafetyFragment"  // 안전 자료
+        val REWARDS_FRAGMENT = "RewardsFragment"  // 리워드 사용 화면
+        val REWARDSHISTORY_FRAGMENT = "RewardsHistoryFragment"  // 리워드 히스토리 화면
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,30 +39,33 @@ class MainActivity : AppCompatActivity() {
 
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
-        replaceFragment(MAIN_FRAGMENT, false, null)
+        replaceFragment(SAFETY_FRAGMENT, false, null)
 
     }
 
     // 지정한 Fragment를 보여주는 메서드
-    fun replaceFragment(name:String, addToBackStack:Boolean, bundle: Bundle?){
+    fun replaceFragment(name: String, addToBackStack: Boolean, bundle: Bundle?) {
         // Fragment 교체 상태로 설정한다.
         val fragmentTransaction = supportFragmentManager.beginTransaction()
 
         // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
-        if(newFragment != null){
+        if (newFragment != null) {
             oldFragment = newFragment
         }
 
         // 새로운 Fragment를 담을 변수
-        newFragment = when(name){
+        newFragment = when (name) {
             MAIN_FRAGMENT -> MainFragment()
             PARTICIPATE_FRAGMENT -> ParticipateFragment()
+            SAFETY_FRAGMENT -> SafetyFragment()
+            REWARDS_FRAGMENT -> RewardsFragment()
+            REWARDSHISTORY_FRAGMENT -> RewardsHistoryFragment()
             else -> Fragment()
         }
 
         newFragment?.arguments = bundle
 
-        if(newFragment != null) {
+        if (newFragment != null) {
 
             // oldFragment -> newFragment로 이동
             // oldFramgent : exit
@@ -74,7 +76,7 @@ class MainActivity : AppCompatActivity() {
             // newFragment : return
 
             // 애니메이션 설정
-            if(oldFragment != null){
+            if (oldFragment != null) {
                 oldFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
                 oldFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
                 oldFragment?.enterTransition = null
@@ -93,14 +95,12 @@ class MainActivity : AppCompatActivity() {
                 // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
                 fragmentTransaction.addToBackStack(name)
             }
-
             // 교체 명령이 동작하도록 한다.
             fragmentTransaction.commit()
         }
     }
-
     // Fragment를 BackStack에서 제거한다.
-    fun removeFragment(name:String){
+    fun removeFragment(name: String) {
         supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
