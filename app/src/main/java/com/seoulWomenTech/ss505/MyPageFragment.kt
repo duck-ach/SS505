eade8ef45c9c27e2865e5c59d5904b858c078271
iiolo <alohalo98@naver.com>
2023-08-05 19:11:09 +0900

    feat : 마이페이지 데이터 연결, 툴바, 네비게이션 drawer 추가

--- app/src/main/java/com/seoulWomenTech/ss505/MyPageFragment.kt
+++ app/src/main/java/com/seoulWomenTech/ss505/MyPageFragment.kt
@@ -5,58 +5,85 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.seoulWomenTech.ss505.databinding.ActivityMyPageFragmentBinding
+import com.seoulWomenTech.ss505.databinding.FragmentMypageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var binding: MyPageFragment
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-    }
+    lateinit var fragmentMyPageFragmentBinding: FragmentMypageBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
-    }
+        fragmentMyPageFragmentBinding = FragmentMypageBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val userInfo = UserInfoDAO.selectData(mainActivity,mainActivity.userPosition)
+        fragmentMyPageFragmentBinding.run {
+
+            toolbarMyPage.run{
+                title = "Safety Seoul"
+
+                setNavigationOnClickListener {
+                    drawerLayoutMyPage.open()
+
+                }
+
+            }
+
+            navigationViewMyPage.run {
+                setNavigationItemSelectedListener {
+                    when(it.itemId){
+                        R.id.nav_challenge -> {
+                            drawerLayoutMyPage.close()
+                            mainActivity.supportFragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                        }
+
+                        R.id.nav_safedata -> {
+                            mainActivity.replaceFragment(MainActivity.SAFETYDATA_FRAGMENT,true,null)
+                            drawerLayoutMyPage.close()
+                        }
+
+                        R.id.mypage -> {
+                            mainActivity.replaceFragment(MainActivity.MYPAGE_FRAGMENT,true,null)
+                            drawerLayoutMyPage.close()
+                        }
+                        R.id.nav_logout -> {
+                            mainActivity.supportFragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                            mainActivity.replaceFragment(MainActivity.LOGIN_FRAGMENT,false,null)
+
+                        }
+
+                        else -> {
+
+                        }
+
+                    }
+                    false
                 }
             }
+
+
+
+
+            val imgSrc = mainActivity.resources.getIdentifier(
+                userInfo.image,
+                "drawable",
+                mainActivity.packageName
+            )
+            if (imgSrc != null) {
+                myPageUserImg.setImageResource(imgSrc)
+            }
+
+            myPageUserName.text= userInfo.name
+            myPageUserEmail.text= userInfo.email
+
+        }
+
+        return fragmentMyPageFragmentBinding.root
     }
+
 }
